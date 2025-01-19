b8c23281a12223b6c2e54b91f2129d0d2edff818
jkgj <jk.gj@web.de>
2025-01-19 15:12:53 +0100

    test

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -1,7 +1,3 @@
-import org.junit.Assert;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-
 public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
@@ -22,10 +18,16 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
+    int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+    for (int prime : primes) {
+    assertEquals("this will fail", true, false);
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    }
+    /int[] nonPrimes = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30};
+      for (int nonPrime : nonPrimes) {
+    assertEquals("this will fail", true, false);
   }
+}
