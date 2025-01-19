5b5c08abf9d5d7a89ab6bddd90bc2748f0b77a7f
jkgj <jk.gj@web.de>
2025-01-19 15:23:31 +0100

    test

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -24,7 +24,7 @@ public class PrimeCheckTest {
   public void testPrimes() {
     int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
     for (int prime : primes) {
-      assertEquals("Test prime number " + prime, true, PrimeCheck.isPrime(prime));
+      assertEquals("this will fail", true, false);
     }
   }
 
@@ -33,7 +33,7 @@ public class PrimeCheckTest {
   public void testNonPrimes() {
     int[] nonPrimes = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30};
     for (int nonPrime : nonPrimes) {
-      assertEquals("Test non-prime number " + nonPrime, false, PrimeCheck.isPrime(nonPrime));
+      assertEquals("this will fail", true, false);
     }
   }
 }
